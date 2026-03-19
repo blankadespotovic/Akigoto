65d8745f462d4343db5973a16cf026a9ba46853d
deps7 <bdeps18@gmail.com>
2026-03-19 20:02:01 +0100

    Dodavanje tabličnog prikaza

--- src/pages/postignuca/PregledPostignuca.jsx
+++ src/pages/postignuca/PregledPostignuca.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import PostignucaService from "../../services/postignuca/PostignucaService.js";
+import { Table } from "react-bootstrap";
+import { GrValidate } from "react-icons/gr";
 
 export default function PregledPostignuca() {
     const [postignuca, setPostignuca] = useState([]);
@@ -15,23 +17,38 @@ export default function PregledPostignuca() {
     }, []);
 
     return (
-        <ol>
-            {postignuca && postignuca.map((postignuceKategorija) => (
-                <li>
-                    {postignuceKategorija.kategorija}
-                    <ul>
-                        {postignuceKategorija.postignuca.map((postignuce) => (
-                            <li
-                                style={postignuce.zavrseno ? { color: "green" } : { color: "red" }}
-                            >
-                                {postignuce.naziv} - {postignuce.opis} [{postignuce.procjena}]
-                            </li>
-                        )
-                        )}
-                    </ul>
-                </li>
-            )
-            )}
-        </ol>
-    );
+
+        <>
+            <Table style={{ textAlign: 'center' }}>
+                <thead>
+                    <tr>
+                        <th>Naziv postignuća</th>
+                        <th>Opis</th>
+                        <th>Procjena</th>
+                        <th>Završeno</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {postignuca && postignuca.map((kategorija) => (
+                        kategorija.postignuca.map((postignuce) => (
+                            <tr>
+                                <td><i>{postignuce.naziv}</i></td>
+                                <td>{postignuce.opis}</td>
+                                <td>{postignuce.procjena}</td>
+                                <td>
+                                    <GrValidate
+                                        size={25}
+                                        color={postignuce.zavrseno ? 'green' : 'red'}
+                                    />
+                                </td>
+                            </tr>
+                        ))
+                    ))}
+                </tbody>
+
+            </Table>
+        </>
+
+    )
 }
