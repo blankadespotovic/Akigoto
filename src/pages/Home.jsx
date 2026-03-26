3bd3e5024f407c1d2b19ce1c12cbcbf8267d9228
deps7 <bdeps18@gmail.com>
2026-03-26 20:50:14 +0100

    Dodavanje favicona, slike i animacije na stranicu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
 import { IME_APLIKACIJE } from "../constants";
+import shiba from "../assets/shiba.png"
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Home() {
     return (
         <>
             <h1>Dobrodošli u {IME_APLIKACIJE}</h1>
             <div>
-                Akigoto je web aplikacija za učenje japanskog jezika koja spaja vizualno učenje i interaktivne vježbe u zabavu!
-                Naziv <b>明語都 (Akigoto)</b> nosi posebno značenje <i>mjesto gdje jezik postaje jasan</i>. Upravo to je i cilj ove aplikacije, kroz intuitivne i zabavne metode učenja razviti razumijevanje japanskog jezika.
+                <p>Akigoto je web aplikacija za učenje japanskog jezika koja spaja vizualno učenje i interaktivne vježbe u zabavu!
+                Naziv <b>明語都 (Akigoto)</b> nosi posebno značenje <i>mjesto gdje jezik postaje jasan</i>. Upravo to je i cilj ove aplikacije, kroz intuitivne i zabavne metode učenja razviti razumijevanje japanskog jezika.</p>
+            
+               <div style={{display:"flex"}}>
+                 <img src={shiba} style={{maxWidth:"35%", margin:"0 auto"}} />
+               </div>
+            
+            </div>
+            <div style={{maxWidth: '30%', margin: 'auto'}}>
+                <DotLottieReact
+                    src="/KoiFish.lottie"
+
+                    loop
+                    autoplay
+                />
             </div>
         </>
     )
